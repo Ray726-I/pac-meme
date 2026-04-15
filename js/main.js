b70fd859f0018664ef57256285e8d7167a255154
Ray <one579295@gmail.com>
2026-04-15 18:06:32 +0530

    Change overall UI: border/walls and position of scoreboard, with lives shown with hearts

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-const GAME_SIZE = 640;
+const GAME_WIDTH = 640;
+const GAME_HEIGHT = 700;
 
 const config = {
   type: Phaser.AUTO,
   parent: "game-container",
-  width: GAME_SIZE,
-  height: GAME_SIZE,
+  width: GAME_WIDTH,
+  height: GAME_HEIGHT,
   backgroundColor: "#0f172a",
   pixelArt: true,
   physics: {
